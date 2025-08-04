f3d93d051949d87b399bf40d7088d4d26a9502b2
Shaumik Kalwit <shaumik.kalwit@gmail.com>
2025-08-04 13:20:33 -0500

    added cmake

---
+++ build/CMakeFiles/spsc_app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spsc_app.dir/main.cpp.o"
+  "CMakeFiles/spsc_app.dir/main.cpp.o.d"
+  "spsc_app"
+  "spsc_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spsc_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
